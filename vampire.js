891cc47d1ad2ac3a38191bcad82b025cee3ce148
easydoesit <michael.grills@gmail.com>
2023-01-23 06:41:43 +0000

    add 5 functions

--- vampire.js
+++ vampire.js
@@ -10,22 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this;
+    this.offspring.push(vampire);
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfCreators = 0;
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numberOfCreators++;
+    }
+    return numberOfCreators;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
+    
   }
 
   /** Stretch **/
@@ -36,9 +47,52 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this === vampire) {
+      return this;
+    }
+    
+    if (this.name === 'root') {
+      return this;
+    }
+
+    if (this.numberOfVampiresFromOriginal <=  1 || vampire.numberOfVampiresFromOriginal <= 1) {
+      return this.creator;
+    }
+
+    for (let i in this.offspring) {
+      if (this.offspring[i] === vampire)
+        return this;
+    }
+
+    for (let i in vampire.offspring) {
+      if (vampire.offspring[i] === this)
+        return vampire;
+    }
+
+    const thisLine = [];
+    const vampireLine = [];
+    let currentThis = this;
+    let currentVamp = vampire;
+
+    while (currentThis.creator) {
+      thisLine.push(currentThis.creator);
+      currentThis = currentThis.creator;
+    }
+
+    while (currentVamp.creator) {
+      vampireLine.push(currentVamp.creator);
+      currentVamp = currentVamp.creator;
+    }
+
+    for (let i in thisLine) {
+      for (let j in vampireLine) {
+        if (thisLine[i] === vampireLine[j]) {
+          return thisLine[i];
+        }
+      }
+    }
 
   }
 }
 
 module.exports = Vampire;
-
